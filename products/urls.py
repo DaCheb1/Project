4e63054122bbf4005eb007662968d5a2a9b6d2b8
DaCheb1 <cheban_1979@inbox.ru>
2025-06-18 17:31:15 +0300

    Мы создали страницу с информацией о товаре

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from products.views import catalog
+from products.views import catalog, catalog_detail
 urlpatterns = [
-    path('catalog/', catalog, name = 'catalog')
+    path('catalog/', catalog, name = 'catalog'), 
+    path('catalog/<int:pk>/', catalog_detail, name = 'catalog_detail')
 ]
--- products/views.py
+++ products/views.py
@@ -3,3 +3,9 @@ from products.models import Product
 def catalog (request):
     products = Product.objects.all()
     return render(request, 'catalog.html', {'products': products})
+
+def catalog_detail (request, pk):
+    product = Product.objects.get(pk = pk)
+    return render(request, 'catalog_detail.html', {'product': product})
+
+    

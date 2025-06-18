a7df54fc92199f8dda02ae5fb08f5a715988540d
DaCheb1 <cheban_1979@inbox.ru>
2025-06-18 18:01:56 +0300

    Мы сделали характеристики товара

--- electronik/settings.py
+++ electronik/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'products',
+    'django_json_widget',
 ]
 
 MIDDLEWARE = [
--- products/admin.py
+++ products/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from products.models import Category, Product, Manufacturer
-
+from django.db.models import JSONField
+from django_json_widget.widgets import JSONEditorWidget
 admin.site.register(Category)
 admin.site.register(Manufacturer)
 
 @admin.register(Product)
 class ProductAdmin (admin.ModelAdmin):
     list_display = ['title', 'created_at', 'category__title', 'manufacturer__title']
+    formfield_overrides = {JSONField: {'widget': JSONEditorWidget }}
 
     def category__title(self, obj):
         return obj.category.title

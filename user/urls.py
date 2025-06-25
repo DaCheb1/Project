d3c94368d2a76c7431fda2638827043219f2beb8
DaCheb1 <cheban_1979@inbox.ru>
2025-06-25 17:43:25 +0300

    Ну мы создали аутентификацию

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from user.views import *
 urlpatterns = [
-    
+    path('login/',UserLoginView.as_view(),name = 'login'),
+    path('register/',RegisterView.as_view(),name = 'register'),
+    path('account/',AccountView.as_view(),name = 'account'),
+    path('logout/',UserLogoutView.as_view(),name = 'logout')
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from user.forms import RegisterForm
+class UserLoginView(LoginView):
+    template_name = 'login.html'
 
-# Create your views here.
+class RegisterView(CreateView):
+    model = User
+    success_url = reverse_lazy('login')
+    form_class = RegisterForm
+    template_name = 'register.html'
+
+class AccountView(TemplateView):
+    template_name = 'account.html'
+    
+class UserLogoutView(LogoutView):
+    pass
